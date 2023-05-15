f27fb2a65c61daba608801dab70aa3c91cf5bee0
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-05-15 21:01:13 +0700

    add comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import axios from 'axios'
 import UserContextProvider from './context/UserContext'
-import ProfilePage from './pages/ProfilePage.'
+import ProfilePage from './pages/ProfilePage'
 import PlacesPage from './pages/PlacesPage'
 import PlacesFormPage from './pages/PlacesFormPage';
 import PlacePage from './pages/PlacePage'
@@ -26,9 +26,9 @@ const App = () => {
                     <Route path= '/login' element={<LoginPage />} />
                     <Route path= '/register' element={<RegisterPage />} />
                     <Route path= "/account" element={<ProfilePage />} />
-                    <Route path= "/account/places" element={<PlacesPage />} />
-                    <Route path= "/account/places/new" element={<PlacesFormPage />} />
-                    <Route path= "/account/places/:id" element={<PlacesFormPage />} />
+                    <Route path= "/account/places" element={<PlacesPage />} /> {/* dashboard */}
+                    <Route path= "/account/places/new" element={<PlacesFormPage />} /> {/* add */}
+                    <Route path= "/account/places/:id" element={<PlacesFormPage />} /> {/* edit */}
                     <Route path= "/place/:id" element={<PlacePage />} />
                     <Route path= "/account/bookings" element={<BookingsPage />} />
                     <Route path= "/account/bookings/:id" element={<BookingPage />} />
--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -23,7 +23,7 @@ const PlacesFormPage = () => {
         if (!id) {
             return;
         }
-        axios.get('places/'+id).then(res => {
+        axios.get('/places/'+id).then(res => {
             const {data} = res;
             setTitle(data.title);
             setAddress(data.address);
